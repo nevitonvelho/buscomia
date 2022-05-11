028986831acc6bc7df2127342e45e0bb5b781fdf
Your Name <nevitonvelho01@gmail.com>
2022-05-11 12:15:52 -0300

    busconomia

---
+++ layouts/default.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+          busconomia
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav>
+
+    <section>kdfh</section>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .navbar{
+    background-color: #f46036;
+
+    a{
+      color: #fff;
+    }
+  }
+</style>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
